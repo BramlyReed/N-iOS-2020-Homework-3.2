a66460087bd26a351626f21f5dc96d77fdadb763
BramlyReed <ya.andreev99@ya.ru>
2020-11-25 07:49:41 -0800

    Были загружены файлы, которые не загрузились в первый раз

--- Homework 3.2/ViewController.swift
+++ Homework 3.2/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let nlbl = UILabel()
+    var counter = 0
+    let vw = UIView()
+    private lazy var slideToSwipe: UIPanGestureRecognizer = UIPanGestureRecognizer(
+        target: self,
+        action: #selector(slideToSwipeF(_:))
+    )
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newLabel()
     }
-
-
+    func newLabel(){
+        nlbl.backgroundColor = .green
+        nlbl.textAlignment = NSTextAlignment.center
+        nlbl.text = String(counter)
+        nlbl.center.x = self.view.center.x
+        nlbl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nlbl)
+        let topConstraint1 = nlbl.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        let leftConstraint1 = nlbl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        let rightConstraint1 = nlbl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        let height1 = nlbl.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        vw.backgroundColor = .blue
+        vw.center.x = self.view.center.x
+        vw.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(vw)
+        let topConstraint2 = vw.topAnchor.constraint(equalTo: nlbl.bottomAnchor, constant: 40).isActive = true
+        let leftConstraint2 = vw.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        let rightConstraint2 = vw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        let height2 = vw.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        vw.addGestureRecognizer(slideToSwipe)
+        vw.isUserInteractionEnabled = true
+    }
+    
+    @objc func slideToSwipeF(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            if vw.frame.contains(sender.location(in: self.view)) {
+                counter = counter - Int(sender.translation(in: vw).y)
+                nlbl.text = String(counter)
+            }
+        default:
+            break
+        }
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    }
 }
 
